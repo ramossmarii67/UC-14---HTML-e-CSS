113a9c45d81c571cbfc48d84a56b9d33fd2d6018
ramossmarii67 <mariaclararamosdonscto.2010@gmail.com>
2026-09-17 10:34:00 -0300

    Add files via upload

---
+++ UC15 - DOM (17-09)/script.js
@@ -0,0 +1,10 @@
+function mudartexto() {
+    document.getElementById
+    ("titulo").textContent = "Você clicou no botão!";
+}
+
+function mudarcor() {
+    document.getElementById("mensagem").style.color =
+    "black";
+}
+
